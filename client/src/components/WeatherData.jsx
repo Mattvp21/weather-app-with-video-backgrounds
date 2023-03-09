106702f8456a74b45f8f4d9ac49111ec5c60244d
Matthew Van Pelt <74370212+Mattvp21@users.noreply.github.com>
2023-03-08 19:57:13 -0500

    font change

--- client/src/components/WeatherData.jsx
+++ client/src/components/WeatherData.jsx
@@ -11,39 +11,39 @@ function WeatherData({weatherData, open}) {
                 <div className='content-group'>
                     <div className='content-group__data'>
                         <h2>TEMP</h2>
-                        <p>{weatherData.main.temp}</p>
+                        <p className='data'>{weatherData.main.temp}</p>
                     </div>
                     <div className='content-group__data'>
                         <h2>FEELS LIKE</h2>
-                        <p>{weatherData.main.feels_like}</p>
+                        <p className='data'>{weatherData.main.feels_like}</p>
                     </div>
                     <div className='content-group__data'>
                         <h2>SKY</h2>
-                        <p>{weatherData.weather[0].description}</p>
+                        <p className='data'>{weatherData.weather[0].description}</p>
                     </div>
                 </div>
                 <div className='content-group'>
                 <div className='content-group__data'>
                         <h2>MIN</h2>
-                        <p>{weatherData.main.temp_min}</p>
+                        <p className='data'>{weatherData.main.temp_min}</p>
                     </div>
                     <div className='content-group__data'>
                         <h2>MAX</h2>
-                        <p>{weatherData.main.temp_max}</p>
+                        <p className='data'>{weatherData.main.temp_max}</p>
                     </div>
                     <div className='content-group__data'>
                         <h2>HUMDITY</h2>
-                        <p>{weatherData.main.humidity}%</p>
+                        <p className='data'>{weatherData.main.humidity}%</p>
                     </div>
                 </div>
                 <div className='content-group'>
                 <div className='content-group__data'>
                         <h2>WIND SPEED</h2>
-                        <p>{weatherData.wind.speed}MPH</p>
+                        <p className='data'>{weatherData.wind.speed}MPH</p>
                     </div>
                     <div className='content-group__data'>
-                        <h2>GUST</h2>
-                        <p>{weatherData.wind.gust}MPH</p>
+                        <h2>VISIBILITY</h2>
+                        <p className='data'>{weatherData.visibility}</p>
                     </div>
                     
                 </div>
